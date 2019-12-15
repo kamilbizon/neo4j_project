2ee637fa2e92ff952ee46777259f5e3a01e20195
kamil <kamilbizon@outlook.com>
2019-12-15 20:29:57 +0100

    cleanup

--- sqlite.sql
+++
@@ -1,6 +0,0 @@
-create table pomiar (urzadzenie TEXT, dane BLOB, data TEXT, id INTEGER PRIMARY KEY);
-insert into notatki (urzadzenie, dane, data) values
-("jedzenie", "smaczny obiad", datetime('now'));
-insert into notatki (urzadzenie, dane, data) values
-("praca", "zadanie", datetime('now'));
-select * from notatki;
